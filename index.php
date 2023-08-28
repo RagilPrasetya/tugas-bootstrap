ca550cc0d6a4d6d7d36a7b91080707a82a06d436
RagilPrasetya <110957195+RagilPrasetya@users.noreply.github.com>
2023-08-28 07:55:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.rtl.min.css" integrity="sha384-PRrgQVJ8NNHGieOA1grGdCTIt4h21CzJs6SnWH4YMQ6G5F5+IEzOHz67L4SQaF0o" crossorigin="anonymous">
+    
+
+    <title>Home</title>
+</head>
+<body id="home">
+     <!-- Navbar -->
+     <nav class="bg-primary navbar navbar-expand-lg shadow-sm navbar-light fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">Ragil Prasetya</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#project">Project</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+    <!-- end navbar -->
+
+    <!-- jumbotron -->
+    <section class="jumbotron text-center">
+        <img src="img/ishimi.jpg" alt="idwndini" width="200" class="rounded-circle img-thumbnail" >
+        <h1 class="display-4">Ragil Prasetya</h1>
+        <p class="lead">Siswa | Junior Programmer</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffff" fill-opacity="1" d="M0,160L15,144C30,128,60,96,90,101.3C120,107,150,149,180,181.3C210,213,240,235,270,213.3C300,192,330,128,360,112C390,96,420,128,450,138.7C480,149,510,139,540,138.7C570,139,600,149,630,165.3C660,181,690,203,720,186.7C750,171,780,117,810,122.7C840,128,870,192,900,208C930,224,960,192,990,186.7C1020,181,1050,203,1080,197.3C1110,192,1140,160,1170,133.3C1200,107,1230,85,1260,112C1290,139,1320,213,1350,224C1380,235,1410,181,1425,154.7L1440,128L1440,320L1425,320C1410,320,1380,320,1350,320C1320,320,1290,320,1260,320C1230,320,1200,320,1170,320C1140,320,1110,320,1080,320C1050,320,1020,320,990,320C960,320,930,320,900,320C870,320,840,320,810,320C780,320,750,320,720,320C690,320,660,320,630,320C600,320,570,320,540,320C510,320,480,320,450,320C420,320,390,320,360,320C330,320,300,320,270,320C240,320,210,320,180,320C150,320,120,320,90,320C60,320,30,320,15,320L0,320Z"></path></svg>
+    </section>
+    <!-- end jumbotron -->
+    <!-- About -->
+    <section id="about">
+    <div class="container">
+        <div class="row text-center mb-3">
+            <div class="col">
+                <h2>About Me</h2>
+            </div>
+        </div>
+            <div class="row justify-content-center fs-5 text-center">
+                <div class="col-4">
+                    <p>Hanya siswa biasa</p>
+                </div>
+                <div class="col-4">
+                    <p>Hanya Siswa Biasa</p>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#c2e0f6" fill-opacity="1" d="M0,32L17.1,69.3C34.3,107,69,181,103,186.7C137.1,192,171,128,206,122.7C240,117,274,171,309,165.3C342.9,160,377,96,411,90.7C445.7,85,480,139,514,133.3C548.6,128,583,64,617,53.3C651.4,43,686,85,720,133.3C754.3,181,789,235,823,229.3C857.1,224,891,160,926,154.7C960,149,994,203,1029,208C1062.9,213,1097,171,1131,170.7C1165.7,171,1200,213,1234,208C1268.6,203,1303,149,1337,138.7C1371.4,128,1406,160,1423,176L1440,192L1440,320L1422.9,320C1405.7,320,1371,320,1337,320C1302.9,320,1269,320,1234,320C1200,320,1166,320,1131,320C1097.1,320,1063,320,1029,320C994.3,320,960,320,926,320C891.4,320,857,320,823,320C788.6,320,754,320,720,320C685.7,320,651,320,617,320C582.9,320,549,320,514,320C480,320,446,320,411,320C377.1,320,343,320,309,320C274.3,320,240,320,206,320C171.4,320,137,320,103,320C68.6,320,34,320,17,320L0,320Z"></path></svg>
+    </section>
+    <!-- end About -->
+
+    <!-- Project -->
+    <section id="project">
+       <div class="container">
+            <div class="row text-center mb-4">
+                <div class="col">
+                    <h2>My Project</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4 mb-3">
+                <div class="card">
+                    <img src="img/ishimi.jpg" class="card-img-top" alt="ishimi">
+                    <div class="card-body">
+                        <p class="card-text">project 1</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-3">
+                <div class="card">
+                    <img src="img/ishimi.jpg" class="card-img-top" alt="ishimi">
+                    <div class="card-body">
+                        <p class="card-text">project 2</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-3">
+                <div class="card">
+                    <img src="img/ishimi.jpg" class="card-img-top" alt="ishimi">
+                    <div class="card-body">
+                        <p class="card-text">project 3</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-3">
+                <div class="card">
+                    <img src="img/ishimi.jpg" class="card-img-top" alt="ishimi">
+                    <div class="card-body">
+                        <p class="card-text">project 4</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-3">
+                <div class="card">
+                    <img src="img/ishimi.jpg" class="card-img-top" alt="ishimi">
+                    <div class="card-body">
+                        <p class="card-text">project 5</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-3">
+                <div class="card">
+                    <img src="img/ishimi.jpg" class="card-img-top" alt="ishimi">
+                    <div class="card-body">
+                        <p class="card-text">project 6</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       </div>
+       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,64L15,80C30,96,60,128,90,117.3C120,107,150,53,180,69.3C210,85,240,171,270,202.7C300,235,330,213,360,197.3C390,181,420,171,450,160C480,149,510,139,540,160C570,181,600,235,630,240C660,245,690,203,720,192C750,181,780,203,810,186.7C840,171,870,117,900,117.3C930,117,960,171,990,208C1020,245,1050,267,1080,250.7C1110,235,1140,181,1170,149.3C1200,117,1230,107,1260,138.7C1290,171,1320,245,1350,245.3C1380,245,1410,171,1425,133.3L1440,96L1440,320L1425,320C1410,320,1380,320,1350,320C1320,320,1290,320,1260,320C1230,320,1200,320,1170,320C1140,320,1110,320,1080,320C1050,320,1020,320,990,320C960,320,930,320,900,320C870,320,840,320,810,320C780,320,750,320,720,320C690,320,660,320,630,320C600,320,570,320,540,320C510,320,480,320,450,320C420,320,390,320,360,320C330,320,300,320,270,320C240,320,210,320,180,320C150,320,120,320,90,320C60,320,30,320,15,320L0,320Z"></path></svg>
+    </section>
+    <!-- end Project -->
+
+    <!-- Contact -->
+    <section id="contact">
+        <div class="container">
+            <div class="row text-center mb-4">
+                <div class="col">
+                    <h2>Contact Me</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <form>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nama Lengkap</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="Pesan" class="form-label">Example textarea</label>
+                            <textarea class="form-control" id="pesan" rows="3"></textarea>
+                        </div>
+                            <button type="submit" class="btn btn-primary">Kirim</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,32L17.1,37.3C34.3,43,69,53,103,85.3C137.1,117,171,171,206,208C240,245,274,267,309,234.7C342.9,203,377,117,411,112C445.7,107,480,181,514,197.3C548.6,213,583,171,617,138.7C651.4,107,686,85,720,96C754.3,107,789,149,823,149.3C857.1,149,891,107,926,112C960,117,994,171,1029,176C1062.9,181,1097,139,1131,122.7C1165.7,107,1200,117,1234,138.7C1268.6,160,1303,192,1337,186.7C1371.4,181,1406,139,1423,117.3L1440,96L1440,320L1422.9,320C1405.7,320,1371,320,1337,320C1302.9,320,1269,320,1234,320C1200,320,1166,320,1131,320C1097.1,320,1063,320,1029,320C994.3,320,960,320,926,320C891.4,320,857,320,823,320C788.6,320,754,320,720,320C685.7,320,651,320,617,320C582.9,320,549,320,514,320C480,320,446,320,411,320C377.1,320,343,320,309,320C274.3,320,240,320,206,320C171.4,320,137,320,103,320C68.6,320,34,320,17,320L0,320Z"></path></svg>
+    </section>
+    <!-- end Contact -->
+
+    <!-- Footer -->
+    <footer class="bg-primary text-white text-center pb-5">
+        <p class="fs-2">Created by Ragil Prasetya</p>
+    </footer>
+    <!-- end Footer -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+  
+</body>
+</html>
